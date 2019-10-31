49416795adf1ffa634d8c9deb384cb553929cb73
stravze <stravze@gmail.com>
2019-10-31 14:14:13 +0000

    adding addtional nics to palo prod and stage

--- 00-init.tf
+++ 00-init.tf
@@ -10,9 +10,3 @@ provider "azurerm" {
 
 # Additional Providers Required to access to different Subscriptons within HMCTS Account
 
-
-provider "azurerm" {
-  alias                             = "dcd-cftapps-sbox"
-  version                           = ">=1.24.0"
-  subscription_id                   = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
-}
--- 25-firewalls.tf
+++ 25-firewalls.tf
@@ -4,12 +4,12 @@ resource "azurerm_network_interface" "ansible_server_nic" {
   location                            = "${data.azurerm_resource_group.rg-dmz.location}"
   resource_group_name                 = "${data.azurerm_resource_group.rg-dmz.name}"
 
-    ip_configuration {
-        name                          = "${data.azurerm_virtual_network.vnet-dmz.name}-${var.environment}-ansible-ip"
-        subnet_id                     = "${data.azurerm_subnet.subnet-dmz-mgmt.id}"
-        private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.pip-ansible.id}"
-    }
+  ip_configuration {
+    name                              = "${data.azurerm_virtual_network.vnet-dmz.name}-${var.environment}-ansible-ip"
+    subnet_id                         = "${data.azurerm_subnet.subnet-dmz-mgmt.id}"
+    private_ip_address_allocation     = "dynamic"
+    public_ip_address_id              = "${azurerm_public_ip.pip-ansible.id}"
+  }
 }
 
 resource "azurerm_public_ip" "pip-ansible" {
@@ -35,4 +35,55 @@ module "firewall" {
   ansible-nic                         = "${azurerm_network_interface.ansible_server_nic.id}"
   proxy_subnet                        = "${data.azurerm_subnet.subnet-dmz-proxy.address_prefix}"
   proxy_ip                            = "${data.azurerm_network_interface.proxy-ip.private_ip_address}"
+}
+
+
+resource "azurerm_network_interface" "palo_nic_prod" {
+  name                                = "palo-nic-prod-${count.index}"
+  location                            = "${data.azurerm_resource_group.rg-dmz.location}"
+  resource_group_name                 = "${data.azurerm_resource_group.rg-dmz.name}"
+  enable_ip_forwarding                = "true"
+  count                               = "${var.environment == "prod-intsvc" ? 1 : 0}" 
+
+  ip_configuration {
+    name                              = "palo-nic-prod-${count.index}"
+    subnet_id                         = "${data.azurerm_subnet.subnet-dmz-palo-public.id}"
+    private_ip_address_allocation     = "dynamic"
+    public_ip_address_id              = "${azurerm_public_ip.palo_pip_prod.id}"
+  }
+}
+
+resource "azurerm_public_ip" "palo_pip_prod" { 
+  name                                = "palo-pip-prod-${count.index}"
+  location                            = "${data.azurerm_resource_group.rg-dmz.location}"
+  resource_group_name                 = "${data.azurerm_resource_group.rg-dmz.name}"
+  domain_name_label                   = "palo-pip-prod-${count.index}"
+  allocation_method                   = "Static"
+  sku                                 = "Standard"	
+  count                               = "${var.environment == "prod-intsvc" ? 1 : 0}" 
+}
+
+resource "azurerm_network_interface" "palo_nic_stage" {
+  name                                = "palo-nic-stage-${count.index}"
+  location                            = "${data.azurerm_resource_group.rg-dmz.location}"
+  resource_group_name                 = "${data.azurerm_resource_group.rg-dmz.name}"
+  enable_ip_forwarding                = "true"
+  count                               = "${var.environment == "prod-intsvc" ? 1 : 0}" 
+
+  ip_configuration {
+    name                              = "palo-nic-stage-${count.index}"
+    subnet_id                         = "${data.azurerm_subnet.subnet-dmz-palo-public.id}"
+    private_ip_address_allocation     = "dynamic"
+    public_ip_address_id              = "${azurerm_public_ip.palo_pip_stage.id}"
+  }
+}
+
+resource "azurerm_public_ip" "palo_pip_stage" { 
+  name                                = "palo-pip-stage-${count.index}"
+  location                            = "${data.azurerm_resource_group.rg-dmz.location}"
+  resource_group_name                 = "${data.azurerm_resource_group.rg-dmz.name}"
+  domain_name_label                   = "palo-pip-stage-${count.index}"
+  allocation_method                   = "Static"
+  sku                                 = "Standard"	
+  count                               = "${var.environment == "prod-intsvc" ? 1 : 0}" 
 }
